4b66ccdec171c6a56f4544e7f9b58bdbb558b6bc
seo <kazu22002@gmail.com>
2019-09-12 19:12:12 +0900

    update. Isucon2 disable path.

--- isucon2/files/home/isucon/env.sh
+++ isucon2/files/home/isucon/env.sh
@@ -3,10 +3,11 @@
 HOME=/home/isucon
 NODEPATH=$HOME/local/node-v6.11/bin
 PERLPATH=$HOME/local/perl-5.16/bin
-RUBYPATH=$HOME/local/ruby-1.9/bin
-PYTHONPATH=$HOME/local/python-2.7/bin
-JAVAPATH=$HOME/local/java-6/bin
-PATH=$JAVAPATH:$PYTHONPATH:$RUBYPATH:$PERLPATH:$NODEPATH:$PATH:$HOME/bin
+#RUBYPATH=$HOME/local/ruby-1.9/bin
+#PYTHONPATH=$HOME/local/python-2.7/bin
+#JAVAPATH=$HOME/local/java-6/bin
+#PATH=$JAVAPATH:$PYTHONPATH:$RUBYPATH:$PERLPATH:$NODEPATH:$PATH:$HOME/bin
+PATH=$PERLPATH:$NODEPATH:$PATH:$HOME/bin
 
 export PATH
 exec "$@"
